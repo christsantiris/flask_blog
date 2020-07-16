5b00552db22346cedd90eea9ad50cd73fe2322b3
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-07-15 20:14:00 -0400

    add about route

---
+++ README.md
@@ -0,0 +1,3 @@
+## To run the app in debug mode
+
+### `export FLASK_APP=flaskblog.py` and `export FLASK_DEBUG=1` (or on window `set FLASK_app=flaskblog.py` and `set FLASK_DEBUG=1`) then `flask run`
--- flaskblog.py
+++ flaskblog.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World"
+    return "<h1>Hello World</h1>"
+
+@app.route('/about')
+def about():
+    return "<h1>About</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True) 
