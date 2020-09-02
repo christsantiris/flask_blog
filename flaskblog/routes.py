1a354ba163685dd0ce3aa795b430286535a593a1
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-09-02 13:01:31 -0400

    Update user info and profile pic

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,10 +1,12 @@
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app
 from flaskblog import db
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flaskblog import bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+import os
 
 posts = [
     {
@@ -65,9 +67,33 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account')
+# make sure profile pic has unique filename when saved
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    form_picture.save(picture_path)
+
+    return picture_fn
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
 
